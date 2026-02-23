4e0e7bd39cf08916c99809e35d45f9dd3145cb71
Dieudonné Tchamou <tchafaber86@gmail.com>
2026-02-23 22:47:33 +0100

    Use server supabase client in admin page

--- app/admin/user/page.tsx
+++ app/admin/user/page.tsx
@@ -1,4 +1,9 @@
-import { supabase } from "../../../lib/supabase";
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.SUPABASE_SERVICE_ROLE_KEY!
+);
 
 // TEST CHANGE
 
